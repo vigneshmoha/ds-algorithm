55defee06cf7f49a4b7ebb4d903d94fbec38045f
vigneshmoha <vigneshmoha@gmail.com>
2019-09-30 00:13:30 +0530

    ds-algorithm - Intial commit - Added custom lists aka dynamic array

---
+++ src/csharp/ds.algorithms/ds.algorithms.tests/CustomListTests.cs
@@ -0,0 +1,56 @@
+using System;
+using ds.algorithms.DataStructures;
+using Xunit;
+
+namespace ds.algorithms.tests
+{
+    public class CustomListTests
+    {
+        [Fact]
+        public void CustomLists_ValidateListCount()
+        {
+            var listObj = new CustomList<int>();
+
+            Assert.True(listObj.Count == 0);
+
+            listObj.Add(2);
+            listObj.Add(3);
+            listObj.Add(6);
+            listObj.Add(2);
+            listObj.Add(3);
+            listObj.Add(6);
+
+            Assert.True(listObj.Count == 6);
+        }
+
+        [Fact]
+        public void CustomLists_ValidateClearMethod()
+        {
+            var listObj = new CustomList<int>();
+            listObj.Add(2);
+            listObj.Add(3);
+            listObj.Add(6);
+
+            Assert.True(listObj.Count == 3);
+
+            listObj.Clear();
+
+            Assert.True(listObj.Count == 0);
+        }
+
+        [Fact]
+        public void CustomLists_ValidateRemoveMethod()
+        {
+            var listObj = new CustomList<int>();
+            listObj.Add(2);
+            listObj.Add(3);
+            listObj.Add(6);
+
+            Assert.True(listObj.Remove(3));
+            Assert.True(listObj.Count == 2);
+            Assert.True(listObj[1] == 6);
+
+            Assert.False(listObj.Remove(5));
+        }
+    }
+}
---
+++ src/csharp/ds.algorithms/ds.algorithms/DataStructures/CustomList.cs
@@ -0,0 +1,110 @@
+﻿using System;
+
+namespace ds.algorithms.DataStructures
+{
+    public class CustomList<T>
+    {
+        private T[] _array;
+        private int _size = 0;
+        private int _maxCapacity = 0;
+
+        public int Count => _size;
+
+        public CustomList()
+        {
+            _array = new T[10];
+        }
+
+        public T this[int index]
+        {
+            get
+            {
+                if (index < 0 || index >= _size)
+                {
+                    throw new IndexOutOfRangeException();    
+                }
+
+                return _array[index];
+            }
+
+            set
+            {
+                if (index < 0 || index >= _size)
+                {
+                    throw new IndexOutOfRangeException();
+                }
+
+                _array[index] = value;
+            }
+        }
+
+        #region ListOperations
+
+        public void Add(T value)
+        {
+            if (_size == _maxCapacity)
+            {
+                _maxCapacity = _maxCapacity == 0 ? 4 : _maxCapacity * 2;
+                Array.Resize(ref _array, _maxCapacity);
+            }
+
+            _array[_size] = value;
+            _size++;
+        }
+
+        public void Clear()
+        {
+            if (_size > 0)
+            {
+                Array.Clear(_array, 0, _size);
+                _size = 0;
+            }
+        }
+
+        public int IndexOf(T value)
+        {
+            return Array.IndexOf(_array, value, 0, _size);
+        }
+
+        public bool Remove(T value)
+        {
+            var index = IndexOf(value);
+
+            if (index < 0) return false;
+
+            RemoveAt(index);
+            return true;
+        }
+
+        public void RemoveAt(int index)
+        {
+            if (index < 0 || index > _size)
+            {
+                throw new IndexOutOfRangeException();
+            }
+
+            _size--;
+
+            if (index < _size)
+            {
+                Array.Copy(_array, index + 1, _array, index, _size - index);
+            }
+
+            _array[_size] = default(T);
+        }
+
+        public override string ToString()
+        {
+            var output = string.Empty;
+
+            for (int i = 0; i < _size - 1; i++)
+            {
+                output = output + _array[i] + ',';
+            }
+
+            return output + _array[_size - 1];
+        }
+
+        #endregion
+    }
+}
