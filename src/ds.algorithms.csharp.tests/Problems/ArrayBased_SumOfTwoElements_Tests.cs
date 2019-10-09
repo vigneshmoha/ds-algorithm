42a3bac4d83c334e19b63e60f048f40cc01d8bd5
vigneshmoha <vigneshmoha@gmail.com>
2019-10-09 16:03:24 +0530

    feat: added simple array based problem and its test

---
+++ src/ds.algorithms.csharp.tests/Problems/ArrayBased_SumOfTwoElements_Tests.cs
@@ -0,0 +1,20 @@
+﻿using ds.algorithms.csharp.Problems.ArrayBased;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace ds.algorithms.csharp.tests.Problems
+{
+    public class ArrayBased_SumOfTwoElements_Tests
+    {
+        [Theory]
+        [InlineData(new int[] { 2, 7, 8, 5 }, 10, new int[] {0, 2})]
+        public static void ArrayBased_SumOfTwoElements_PositiveScenarios(int[] inputArray, int target, int[] expectedArray)
+        {
+            var outputArray = SumOfTwoElements.GetIndexOfElements(inputArray, target);
+
+            Assert.Equal(expectedArray, outputArray);
+        }
+    }
+}
---
+++ src/ds.algorithms.csharp/Problems/ArrayBased/SumOfTwoElements.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+
+namespace ds.algorithms.csharp.Problems.ArrayBased
+{
+    public static class SumOfTwoElements
+    {
+        public static int[] GetIndexOfElements(int[] inputArray, int target)
+        {
+            int[] output = new int[2];
+            Dictionary<int, int> hashMap = new Dictionary<int, int> {
+                { inputArray[0], 0 }
+            };
+            for (int i=0; i<inputArray.Length; i++)
+            {
+                var next = target - inputArray[i];
+
+                if(hashMap.ContainsKey(next))
+                {
+                    output[0] = hashMap[next];
+                    output[1] = i;
+                } else
+                {
+                    hashMap[inputArray[i]] = i;
+                }
+            }
+
+            return output;
+        }
+    }
+}
